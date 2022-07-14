ca06d8f7cd36153162e403111425cc4fcec62c97
Antoine Drabble <antoine.drabble@gmail.com>
2022-07-14 12:37:23 +0200

    Optimize revalidate

--- web/pages/api/revalidate.ts
+++ web/pages/api/revalidate.ts
@@ -9,32 +9,37 @@ export default async function handler(req: any, res: any) {
   }
 
   try {
-    await res.unstable_revalidate('/');
-    await res.unstable_revalidate('/about');
-    await res.unstable_revalidate('/contact');
-    await res.unstable_revalidate('/news');
-    await res.unstable_revalidate('/tickets');
-    await res.unstable_revalidate('/info');
+    const revalidate = [
+     () => res.unstable_revalidate('/'),
+     () => res.unstable_revalidate('/about'),
+     () => res.unstable_revalidate('/contact'),
+     () => res.unstable_revalidate('/news'),
+     () => res.unstable_revalidate('/tickets'),
+     () => res.unstable_revalidate('/info')
+    ];
+    
 
     console.log(`Fetching bands`);
-    const bandsResponse = await fetch(`${URL}/api/bands?populate=*`);
+    const bandsResponse = await fetch(`${URL}/api/bands`);
     const { data: bands } = await bandsResponse.json();
     let years = [...new Set(bands.map((band: IBand) => band.attributes.year))];
     console.log(`Revalidation years ${years}`);
     for(const year of years){
       console.log(`Revalidate lineup ${year}`);
-      await res.unstable_revalidate(`/lineup/${year}`);
+      revalidate.push(res.unstable_revalidate(`/lineup/${year}`));
     }
 
     console.log(`Fetching galleries`);
-    const galleriesResponse = await fetch(`${URL}/api/galleries?populate=*`);
+    const galleriesResponse = await fetch(`${URL}/api/galleries`);
     const { data: galleries } = await galleriesResponse.json();
     years = [...new Set(galleries.map((gallery: IGallery) => gallery.attributes.year))];
     console.log(`Revalidation years ${years}`);
     for(const year of years){
       console.log(`Revalidate gallery ${year}`);
-      await res.unstable_revalidate(`/galleries/${year}`);
+      revalidate.push(() => res.unstable_revalidate(`/galleries/${year}`));
     }
+
+    await Promise.all(revalidate);
 
     return res.json({ revalidated: true });
   } catch (err) {
